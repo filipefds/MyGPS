52d77fbf1908bb982a282f0b3b8f703c2534a87c
filipefds <contato.filipefreitas@gmail.com>
2017-08-27 22:01:03 -0300

    implementando GoogleApiClient

--- app/src/main/java/pooa20171/iff/br/mygps/MainActivity.java
+++ app/src/main/java/pooa20171/iff/br/mygps/MainActivity.java
@@ -1,13 +1,33 @@
 package pooa20171.iff.br.mygps;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
